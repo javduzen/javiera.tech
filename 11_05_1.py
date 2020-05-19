d5860dca8b17ccebb61d67502a78ba8819db5930
javduzen <53247682+javduzen@users.noreply.github.com>
2020-05-19 18:33:25 -0400

    Add files via upload

---
+++ 11_05_1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 20:57:04 2020
+
+@author: Javiera
+
+
+Ciclo for ---> estructura
+
+for (rango):
+    instrucciones
+
+
+funcion range (start, stop, step)
+
+La función range nos entrega una colección de elementos que van desde 
+el valor inicial start (o 0 si no está definido), 
+hasta el valor stop - 1 (stop no incluido), 
+avanzando con paso step (o 1 si no está definido)
+
+1. Crear programa que imprima numeros desde el 10 al 15
+1.1 Crear programa que imprima numeros desde el 10 al 36, de 4 en 4
+
+2. Crear tabla de valores de variables
+3. crear programa que imprima numeros todos los numeros pares entre el 0 y el 10
+
+"""
+
+
+for i in range(10, 37, 4):
+    print(i)
+
+
+
+
+
+
+
+
+
+
